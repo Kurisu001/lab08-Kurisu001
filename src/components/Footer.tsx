b1fae3e13928b1fc9989e102db31d7dbc947fd71
Kittipong <anonymous51124@gmail.com>
2025-08-01 00:11:34 +0700

    Sawarudo

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="text-secondary text-center p-2 bg-light mt-auto">
+      Copyright © 2026 Kittipong Chaiyayong 670612115
+    </footer>
+  );
+}
